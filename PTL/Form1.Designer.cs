b390dbe09f6cdc9e0105670debd8046fd90fa7fc
LucaLeFich <pq48wkn@eduvaud.ch>
2025-09-29 15:43:35 +0200

    Lecture du fichier csv

--- PTL/Form1.Designer.cs
+++ PTL/Form1.Designer.cs
@@ -1,38 +1,59 @@
-﻿namespace PTL;
-
-partial class Form1
+﻿namespace PTL
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null))
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            formsPlot1.DisplayScale = 1F;
+            formsPlot1.Location = new Point(78, 42);
+            formsPlot1.Name = "formsPlot1";
+            formsPlot1.Size = new Size(640, 363);
+            formsPlot1.TabIndex = 0;
+            formsPlot1.Load += formsPlot1_Load;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(formsPlot1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
-
-    #endregion
 }
--- PTL/Form1.cs
+++ PTL/Form1.cs
@@ -1,9 +1,59 @@
-namespace PTL;
+using ScottPlot;
+using ScottPlot.WinForms;
+using System.Data;
 
-public partial class Form1 : Form
+namespace PTL
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        private DataTable csvData;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string csvPath = @"D:\323- programation fonctionnelle\Projet\323-Plot-those-lines\PTL\data_LeMans_race_winners.csv";
+            csvData = LoadCsvToDataTable(csvPath);
+
+            string columns = string.Join(", ", csvData.Columns.Cast<DataColumn>().Select(c => c.ColumnName));
+            MessageBox.Show("Colonnes disponibles : " + columns);
+
+            double[] years = csvData.AsEnumerable().Select(r => Convert.ToDouble(r["Year"])).ToArray();
+            double[] speeds = csvData.AsEnumerable().Select(r => Convert.ToDouble(r["Average_speed_kmh"])).ToArray();
+
+            var scatter = formsPlot1.Plot.Add.Scatter(years, speeds);
+            formsPlot1.Plot.Axes.Title.Label.Text = "Vitesse moyenne des vainqueurs au Mans";
+            formsPlot1.Plot.Axes.Bottom.Label.Text = "Année";
+            formsPlot1.Plot.Axes.Left.Label.Text = "Vitesse moyenne (km/h)";
+
+            formsPlot1.Refresh();
+        }
+
+        private DataTable LoadCsvToDataTable(string csvPath)
+        {
+            var dt = new DataTable();
+            var lines = File.ReadAllLines(csvPath);
+
+            if (lines.Length > 0)
+            {
+                var headers = lines[0].Split(',');
+                foreach (var header in headers)
+                    dt.Columns.Add(header);
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    var row = lines[i].Split(',');
+                    dt.Rows.Add(row);
+                }
+            }
+            return dt;
+        }
+
+        private void formsPlot1_Load(object sender, EventArgs e)
+        {
+        }
     }
 }
